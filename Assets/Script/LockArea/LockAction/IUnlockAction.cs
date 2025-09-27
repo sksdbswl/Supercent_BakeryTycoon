d0e661be408ccaa87c5e9a88042acdf0efb5972e
노윤지 <>
2025-09-28 04:21:08 +0900

    파티클 수정

--- Assets/Script/LockArea/LockAction/IUnlockAction.cs
+++ Assets/Script/LockArea/LockAction/IUnlockAction.cs
@@ -5,8 +5,6 @@ public interface IUnlockAction
     void Execute(Player player, UnlockTarget[] targets);
 }
 
-// UnlockContext는 사실 필요 없고, 대신 UnlockTarget 배열을 바로 전달하는 방식으로 변경
-// 하지만 유지하고 싶다면 이렇게 확장 가능
 public class UnlockContext
 {
     public UnlockTarget[] Targets;
